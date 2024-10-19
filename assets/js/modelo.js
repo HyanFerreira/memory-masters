8beff95378453528f3c47e83aab196141b575080
Hyan <hcgamesmg2016@gmail.com>
2024-10-18 22:07:30 -0300

    first commit

---
+++ assets/js/modelo.js
@@ -0,0 +1,29 @@
+let modeloHTML = [
+  {
+    id: 1,
+    dataTerm: 'html',
+    text: 'HTML',
+    img: '',
+  },
+  {
+    id: 2,
+    dataTerm: 'html',
+    text: '',
+    img: '../assets/img/memory_masters_one.svg',
+  },
+];
+
+let modeloCSS = [
+  {
+    id: 1,
+    dataTerm: 'css',
+    text: 'CSS',
+    img: '',
+  },
+  {
+    id: 2,
+    dataTerm: 'css',
+    text: '',
+    img: '../assets/img/memory_masters_one.svg',
+  },
+];
